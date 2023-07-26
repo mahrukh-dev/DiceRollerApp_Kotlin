5121fb623bf0043400f2914c674c6dfcb9a6928b
MahRukh <mahrukhdev@gmail.com>
2023-07-26 15:31:50 +0500

    Implementing Functionality of rolling dice in mainActivity using toast

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val dice = Dice(6) //Creates a dice with six sides
+        val rollButton: Button = findViewById(R.id.button) //links with UI button
+        val resultTextView: TextView = findViewById(R.id.textView) //links with UI textView
+        rollButton.setOnClickListener {//calls this lamda function when button is clicked
+            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show() //toast message
+            resultTextView.text = dice.roll().toString() //rolls dice and returns a random number
+        }
     }
 }
