464f8ff4d16901d98a2abf7f99a69e5455707b73
MahRukh <mahrukhdev@gmail.com>
2023-07-26 15:44:21 +0500

    Implementing Functionality of two rolling dice in mainActivity using toast

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.diceroller
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,16 +8,22 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val dice = Dice(6) //Creates a dice with six sides
+        val diceTwo = Dice(6) //Creates a dice with six sides
         val rollButton: Button = findViewById(R.id.button) //links with UI button
-        val resultTextView: TextView = findViewById(R.id.textView) //links with UI textView
+        val resultTextView: TextView = findViewById(R.id.diceOne)
+
+        val resultTextViewTwo: TextView = findViewById(R.id.diceTwo)
+
         rollButton.setOnClickListener {//calls this lamda function when button is clicked
             Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show() //toast message
             resultTextView.text = dice.roll().toString() //rolls dice and returns a random number
+            resultTextViewTwo.text = diceTwo.roll().toString() //rolls dice and returns a random number
         }
     }
 }
