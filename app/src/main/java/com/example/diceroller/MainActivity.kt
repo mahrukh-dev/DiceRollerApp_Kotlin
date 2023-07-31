21ab2d011309935a02c83bb0d532572a4cb1ba67
MahRukh <mahrukhdev@gmail.com>
2023-07-31 14:56:36 +0500

    roll two dices with an image view

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,18 +14,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val dice = Dice(6) //Creates a dice with six sides
-        val diceTwo = Dice(6) //Creates a dice with six sides
         val rollButton: Button = findViewById(R.id.button) //links with UI button
-        val resultTextView: TextView = findViewById(R.id.diceOne)
 
-        val resultTextViewTwo: TextView = findViewById(R.id.diceTwo)
-
-        rollButton.setOnClickListener {//calls this lamda function when button is clicked
-            Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show() //toast message
-            resultTextView.text = dice.roll().toString() //rolls dice and returns a random number
-            resultTextViewTwo.text =
-                diceTwo.roll().toString() //rolls dice and returns a random number
+        rollButton.setOnClickListener{
+            rollDice()
         }
     }
+
+    fun rollDice(){
+
+        val dice = Dice(6) //Creates a dice with six sides
+        val diceTwo = Dice(6) //Creates a dice with six sides
+
+        val resultDiceOne: TextView = findViewById(R.id.diceOne)
+        val resultDiceTwo: TextView = findViewById(R.id.diceTwo)
+
+        val imageDiceOne: ImageView = findViewById(R.id.imageDiceOne)
+        val imageDiceTwo: ImageView = findViewById(R.id.imageDiceTwo)
+
+        //calls this lamda function when button is clicked
+        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show() //toast message
+        resultDiceOne.text = dice.roll().toString() //rolls dice and returns a random number
+        resultDiceTwo.text = diceTwo.roll().toString() //rolls dice and returns a random number
+        when(resultDiceOne.text){
+            "1" -> imageDiceOne.setImageResource(R.drawable.dice_1)
+            "2" -> imageDiceOne.setImageResource(R.drawable.dice_2)
+            "3" -> imageDiceOne.setImageResource(R.drawable.dice_3)
+            "4" -> imageDiceOne.setImageResource(R.drawable.dice_4)
+            "5" -> imageDiceOne.setImageResource(R.drawable.dice_5)
+            "6" -> imageDiceOne.setImageResource(R.drawable.dice_6)
+        }
+        when(resultDiceTwo.text){
+            "1" -> imageDiceTwo.setImageResource(R.drawable.dice_1)
+            "2" -> imageDiceTwo.setImageResource(R.drawable.dice_2)
+            "3" -> imageDiceTwo.setImageResource(R.drawable.dice_3)
+            "4" -> imageDiceTwo.setImageResource(R.drawable.dice_4)
+            "5" -> imageDiceTwo.setImageResource(R.drawable.dice_5)
+            "6" -> imageDiceTwo.setImageResource(R.drawable.dice_6)
+        }
+
+    }
 }
+
